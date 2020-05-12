65df368d2cb7694632cc76fc3b407a3415dd3086
Justin Wilkinson <justin_wilkinson@talktalk.net>
2020-05-12 15:13:44 +0100

    Imlement fence effect

---
+++ Client/Object/ElementCoordinates.cs
@@ -0,0 +1,21 @@
+﻿namespace WelcomeTo.Client.Object
+{
+    public class ElementBoundingRectangle
+    {
+        public double X { get; set; }
+
+        public double Y { get; set; }
+
+        public double Width { get; set; }
+
+        public double Height { get; set; }
+
+        public double Top { get; set; }
+
+        public double Right { get; set; }
+
+        public double Bottom { get; set; }
+
+        public double Left { get; set; }
+    }
+}
