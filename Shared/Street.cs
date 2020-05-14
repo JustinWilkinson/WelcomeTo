31039e00d55eaef279f801da24fe6fd64dd5138a
Justin Wilkinson <justin_wilkinson@talktalk.net>
2020-05-14 09:39:45 +0100

    Move house number computation to street class

--- Shared/Street.cs
+++ Shared/Street.cs
@@ -36,5 +36,80 @@ namespace WelcomeTo.Shared
             }
             return estates.Where(e => e.HouseIndices.Count >=1 && e.HouseIndices.Count <= 6).ToList();
         }
+
+        public IEnumerable<int> GetPossibileNumbersForUnbuiltHouse(House house, NumberEffectPair selectedNumberEffectPair)
+        {
+            if (house.Number.HasValue || selectedNumberEffectPair == null)
+            {
+                yield break;
+            }
+
+            var builtHouses = Houses.Where(x => x.Number.HasValue && x.Index != house.Index);
+            var possibilities = new List<int> { selectedNumberEffectPair.Number };
+
+            if (selectedNumberEffectPair.Effect == CardType.TempAgency)
+            {
+                possibilities.Add(selectedNumberEffectPair.Number - 2);
+                possibilities.Add(selectedNumberEffectPair.Number - 1);
+                possibilities.Add(selectedNumberEffectPair.Number + 1);
+                possibilities.Add(selectedNumberEffectPair.Number + 2);
+            }
+
+            if (!builtHouses.Any())
+            {
+                foreach (var possibility in possibilities)
+                {
+                    yield return possibility;
+                }
+            }
+            else
+            {
+                var nearestHouseLeft = builtHouses.Where(x => x.Index < house.Index).OrderByDescending(x => x.Index).FirstOrDefault();
+                var nearestHouseRight = builtHouses.Where(x => x.Index > house.Index).OrderBy(x => x.Index).FirstOrDefault();
+
+                foreach (var possibility in possibilities)
+                {
+                    if ((nearestHouseLeft == null || possibility > nearestHouseLeft.Number.Value) && (nearestHouseRight == null || possibility < nearestHouseRight.Number.Value))
+                    {
+                        yield return possibility;
+                    }
+                }
+            }
+        }
+
+
+        public IEnumerable<int> GetPossibleBisNumbersForUnbuiltHouse(House house, SelectedHouse selectedHouse)
+        {
+            var houseToTheLeft = Houses.SingleOrDefault(x => x.Index == house.Index - 1);
+            if (houseToTheLeft != null && !houseToTheLeft.FenceBuilt)
+            {
+                if (houseToTheLeft.Number.HasValue)
+                {
+                    yield return houseToTheLeft.Number.Value;
+                }
+                else if (selectedHouse != null && selectedHouse.Street == Position && selectedHouse.Index == houseToTheLeft.Index)
+                {
+                    yield return selectedHouse.Number;
+                }
+            }
+
+            if (!house.FenceBuilt)
+            {
+                var houseToTheRight = Houses.SingleOrDefault(x => x.Index == house.Index + 1);
+                if (houseToTheRight != null)
+                {
+                    if (houseToTheRight.Number.HasValue)
+                    {
+                        yield return houseToTheRight.Number.Value;
+                    }
+                    else if (selectedHouse != null && selectedHouse.Street == Position && selectedHouse.Index == houseToTheRight.Index)
+                    {
+                        yield return selectedHouse.Number;
+                    }
+                }
+            }
+
+            yield break;
+        }
     }
 }
--- Test/Shared/StreetTests.cs
+++ Test/Shared/StreetTests.cs
@@ -1,5 +1,6 @@
 ﻿using NUnit.Framework;
 using System.Collections.Generic;
+using System.Linq;
 using WelcomeTo.Shared;
 using WelcomeTo.Shared.Enumerations;
 
@@ -45,6 +46,50 @@ namespace WelcomeTo.Test.Shared
             Assert.AreEqual(2, estates[2].HouseIndices.Count);
             Assert.AreEqual(10, estates[2].HouseIndices[0]);
             Assert.AreEqual(11, estates[2].HouseIndices[1]);
+        }
+
+        [Test]
+        public void GetPossibileNumbersForUnbuiltHouse_NormalCardSelected_ReturnsCorrectOptions()
+        {
+            // Arrange
+            var street = new Street
+            {
+                Houses = new List<House>()
+                {
+                    new House { Index = 0, Number = 1 },
+                    new House { Index = 1, Number = 2 },
+                    new House { Index = 2, Number = 3 },
+                    new House { Index = 3, Number = null },
+                    new House { Index = 4, Number = 5 },
+                    new House { Index = 5, Number = null },
+                    new House { Index = 6, Number = 9 },
+                    new House { Index = 7, Number = null },
+                    new House { Index = 8, Number = 11 },
+                    new House { Index = 9, Number = null },
+                    new House { Index = 10, Number = 13 },
+                    new House { Index = 11, Number = 14 },
+                }
+            };
+            var numberEffectPair = new NumberEffectPair { Effect = CardType.Park, Number = 8 };
+
+            // Act/Assert
+            foreach (var house in street.Houses)
+            {
+                var possibleHouseNumbers = street.GetPossibileNumbersForUnbuiltHouse(house, numberEffectPair).ToArray();
+
+                if (house.Index == 5)
+                {
+                    Assert.IsTrue(possibleHouseNumbers.Any());
+                    Assert.AreEqual(1, possibleHouseNumbers.Length);
+                    Assert.AreEqual(8, possibleHouseNumbers[0]);
+                }
+                else
+                {
+                    Assert.IsFalse(possibleHouseNumbers.Any());
+                }
+            }
+
+
         }
     }
 }
